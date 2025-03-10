0274f514b568177aa06f0a470a95e25086ddd4e9
Geleta Tamiru <geletatamiru@yahoo.com>
2025-03-10 11:08:37 +0300

    update cartIcon

--- src/components/CartIcon.jsx
+++ src/components/CartIcon.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { toggleCartHidden } from '../redux/cartSlice';
 import shoppingIcon from "../assets/shopping-bag.svg";
 import "./CartIcon.css";
 export default function CartIcon() {
   const dispatch = useDispatch();
+  const itemCount = useSelector(state => state.cart.cartItems.reduce((acc,cartItem) => acc + cartItem.quantity,0))
   return (
     <div className='cart-icon' onClick={() => {dispatch(toggleCartHidden())}}>
       <img src={shoppingIcon} alt="shopping-cart-icon"  className='shopping-icon'/>
-      <span className="item-count">0</span>
+      <span className="item-count">{itemCount}</span>
     </div>
-  )
+  ) 
 }
